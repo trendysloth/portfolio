a60be680e8d876541aa81ec5208d9370f980eb89
deq2 <dequillen@gmail.com>
2022-06-13 12:17:55 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import Container from 'react-bootstrap/Container';
+// import  "./App.css";
+import About from "./About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<main className="text-gray-400 bg-gray-900 body-font">
+			<Container className="p-3">
+				<Container className="p-5 mb-4 bg-light rounded-3">
+					<h1 className="header">
+					Hello
+					</h1>
+					<About />
+				</Container>
+			</Container>
+		</main>
+	)
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Container from 'react-bootstrap/Container';
+// import  "./App.css";
+import About from "./About";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+	return (
+		<main className="text-gray-400 bg-gray-900 body-font">
+			<Container className="p-3">
+				<Container className="p-5 mb-4 bg-light rounded-3">
+					<h1 className="header">
+					Hello
+					</h1>
+					<About />
+				</Container>
+			</Container>
+		</main>
+	)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
