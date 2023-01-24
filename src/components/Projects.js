77a9d52867cb24c34acea5c82b02a85c75b9f64a
Wenying Wu <wwu393@gatech.edu>
2023-01-23 16:09:51 -0800

    beach description updated, projects reordered

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,16 +14,6 @@ function Projects(){
 						<div class="mini-posts">
 							<article class="mini-post">
 								<header>
-									<h3>Restaurant Recommendations</h3>
-									<time> 
-										A restaurant recommendation system built using publicly available Yelp data.
-									</time>
-								</header>
-								<a href="https://trendysloth.github.io/yelp-restaurant-recommender/" class="image"><img src="images/recommendations.png" alt="" /></a>
-							</article>
-
-							<article class="mini-post">
-								<header>
 									<h3>Unfinished Farewell</h3>
 									<time>
 										COVID-19 tribute mural built using React and Javascript
@@ -38,17 +28,41 @@ function Projects(){
 
 							<article class="mini-post">
 								<header>
+									<h3>Restaurant Recommendations</h3>
+									<time> 
+										A restaurant recommendation system built using publicly available Yelp data.
+									</time>
+								</header>
+								<a href="https://trendysloth.github.io/yelp-restaurant-recommender/" class="image"><img src="images/recommendations.png" alt="" /></a>
+							</article>
+
+							<article class="mini-post">
+								<header>
 									<h3>Beach</h3>
 									<time>
-										Virtual Reality application visualizing climate change
+										Beach presents an example of how virtual reality and 360 video could be used to demonstrate environmental data in tangible, visceral ways.
 									</time>
 								</header>
 								<a href="https://player.vimeo.com/video/210814647" class="image">
-									<img src="images/beach.jpeg" alt="" />
+									<img src="images/beach.png" alt="" />
 								</a>
 								<div className="description">
 								</div>
 							</article>
+
+							{/* <article class="mini-post">
+								<header>
+									<h3>AR/VR Podcast</h3>
+									<time>
+										AR/VR Podcast takes the audience through a journey from the past to the present in highlights in AR/ VR technology through data visualization
+									</time>
+								</header>
+								<a href="https://player.vimeo.com/video/210814647" class="image">
+									<img src="images/beach.png" alt="" />
+								</a>
+								<div className="description">
+								</div>
+							</article> */}
 						<Footer />
 						</div>
 					</section>
