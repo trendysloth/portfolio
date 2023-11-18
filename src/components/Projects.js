55a5618f248cacc8778262ff1576b0ed323111b6
Wenying Wu <wwu393@gatech.edu>
2023-11-17 21:51:42 -0500

    Reparaphrasing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -87,30 +87,33 @@ function Projects(){
 								</a>
 							</article>
 							<article class="mini-post">
-								<header>Why did I become a software engineer</header>
+								<header>
+									<h3>Why did I become a software engineer</h3>
 								<time>
-									Why did I become a software engineer?
-
+								
 									I got asked this question so many times at job interviews or sometimes even when I meet a new person. You are an architecture major and why are you a software engineer now? 
 
 									My interest in computation started at an early age when I was in elementary school playing with the LOGO sea turtle drawing software in my computer classes. I always find computers interesting but somehow I 
-									never thought about majoring in computer science in college (I was a math major first year in college but later I switched to architecture because I want to do something more hands on). 
+									never thought about majoring in computer science in college.
 
 									In architecture school, my interest in computer science gradually grew, I was drawn to various design and parametric softwares. Finally, one year I took a CS course that taught me the k-means clustering algorithm. 
 									Somehow I was immediately hooked about how simple and beautiful k-means is. Yes, I want to learn more about computers ... 
 
-									And the rest is the history. I could write another five pages here about my CS journey in grad school. But one thing that I resonated the most with is the Antidisciplinary approach brought up by the MIT media lab.
-									
+									And the rest is the history. I could write another five pages here about my CS journey in grad school. But one thing that I resonated with the most is the Antidisciplinary approach brought up by the MIT media lab.
+									An antidisciplinary project isn't a sum of a bunch of disciplines but something entirely new - the word defies easy definition. It is someone or something that doesn't fit within traditional academic discipline-a field of study with its own particular words, frameworks, and methods.
 
-									<p>
-										An antidisciplinary project isn't a sum of a bunch of disciplines but something entirely new - the word defies easy definition. It is someone or something that doesn't fit within traditional academic discipline-a field of study with its own particular words, frameworks, and methods.
-									</p>
+									So why are we so dogmatic about what we studied in college? The world is becoming increasingly interdisciplinary these days, why can we each adopt our own anti-discinplinary approach?
 
 									Computers are fasincating... Be it LOGO sea turtle, Be it Red Dead Redemption, Be it Neural Networks, Be it Rhinoceros. 
-									Architecture is also fasincating. Be it I.M.Pei, Gothic churches, Bernini, Borromini. Without architecture, where do we go play with our computers?
-									We can't live without computers now. And we have never been able to live without architecture.
+									Architecture is also fasincating... Be it I.M.Pei, Gothic churches, Bernini, Borromini. 
+									
+									Without architecture, where do we go play with our computers?
+									Without computers, how do we make cool architecture?
+
+									We can't live without computers now. Neither have we ever been able to live without architecture.
 									I love both subjects and they are important parts of my life.
 								</time>
+								</header>
 							</article>
 						<Footer />
 						</div>
