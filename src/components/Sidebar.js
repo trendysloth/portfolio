d6811174b095271279bfe0063572614e67a7caa0
Wenying Wu <wwu393@gatech.edu>
2023-11-17 21:30:41 -0500

    Adding why did I become a software engineer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,6 +26,20 @@ function Sidebar(){
 							</p>
 						</section>
 
+						<section class="blurb">
+							<h2>Why did I become a software engineer?</h2>
+							<p> 
+								I got asked this question so many times at job interviews or sometimes even when I meet a new person. You are an architecture major and why are you a software engineer now? 
+								My interest in computation started at an early age when I was in elementary school playing with the LOGO sea turtle drawing software in my computer classes. I always find computers interesting but somehow I 
+								never thought about majoring in computer science in college (I was a math major first year in college but later I switched to architecture because I want to do something more hands on). 
+								
+								In architecture school, my interest in computer science gradually grew, I was drawn to various design and parametric softwares. Finally, one year I took a CS course that taught me the k-means clustering algorithm. 
+								Somehow I was immediately hooked about how simple and beautiful k-means is. Yes, I want to learn more about computers ... 
+								
+								...And the rest is the history. I could write another five pages here about my CS journey in grad school. But hey computers are fasincating... Be it LOGO sea turtle, Be it Red Dead Redemption, Be it Neural Networks, Be it Rhinoceros. We can't live without computers now. They have become part of our lives.
+							</p>
+						</section>
+
 						<section id="footer">
 							<ul class="icons">
 								<li>
